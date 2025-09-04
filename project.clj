f1f4e85786b16ebbc9661a686cbe5c42a59a4d03
Saúl Salinas Treviño <ajiweaver@pm.me>
2025-09-03 23:34:23 -0600

    Added dependencies to project.clj

--- project.clj
+++ project.clj
@@ -4,4 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.bytedeco/mkl-platform-redist "2020.3-1.5.4"]
+                 [uncomplicate/neanderthal "0.43.3"]
                  [quil "3.1.0"]])
